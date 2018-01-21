eb2a326ab351b32e03f76cfa6fc372367a4f6f23
LasaleFamine <info@godev.space>
2018-01-21 11:00:56 +0100

    feat(nginx-proxy-node): added the copy within the nginx sh

--- src/ubuntu/nginx.sh
+++ src/ubuntu/nginx.sh
@@ -7,3 +7,14 @@ sudo apt-get update
 sudo apt-get install nginx -y
 
 echo "Nginx installed ✅"
+echo "Copying sample Nginx proxy configuration for Nodejs server... ⚙️"
+
+sudo cp ../utilities/nginx/nodeserver /etc/nginx/sites-available
+
+echo "You should change the name of the file and the settings inside the file (if you need) and enable it ℹ️"
+echo "Run:"
+echo "mv /etc/nginx/sites-available/nodeserver /etc/nginx/sites-available/theNameYouChoose"
+echo "nano /etc/nginx/sites-available/theNameYouChoose"
+echo "And then enable it"
+echo "ln -s /etc/nginx/sites-available/theNameYouChoose /etc/nginx/sites-enabled/theNameYouChoose"
+
